6f6cad61481f72bc78efcb3a74ea250b63c2a86b
usedhondacivic <micacer48@gmail.com>
2024-08-30 13:44:26 -0400

    Update config

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -8,4 +8,20 @@ return {
 		"christoomey/vim-tmux-navigator",
 		lazy = false,
 	},
+	{
+		"nvimdev/guard.nvim",
+		dependencies = {
+			"nvimdev/guard-collection",
+		},
+		config = function()
+			local ft = require('guard.filetype')
+			ft('typescript,javascript,typescriptreact'):fmt('prettier')
+			ft('python'):fmt('black')
+
+			require('guard').setup({
+				fmt_on_save = true,
+				lsp_as_default_formatter = true,
+			})
+		end,
+	}
 }
