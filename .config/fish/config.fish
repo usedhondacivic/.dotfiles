2b8bf4b1c90bd6a768823ff1102e55a37920d7e7
usedhondacivic <micacer48@gmail.com>
2025-06-01 21:00:07 -0700

    nvim 11.0.2 config update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     source "$HOME/.cargo/env.fish"
-
-    # onshape
-    set ONSHAPE_API https://cad.onshape.com
-    set ONSHAPE_ACCESS_KEY o6FF9OIxU6ewwEqsSvffPkpB
-    set ONSHAPE_SECRET_KEY LKevt9icolcLO9sCqkwCdVdxIJ60sCvmlLdKOY8t13Z2sQA8
+    # config repo
+    alias config='/usr/bin/git --git-dir=/home/michael/.dotfiles/ --work-tree=/home/michael'
 end
 
 # Created by `pipx` on 2024-09-24 21:05:40
-set PATH $PATH /home/michael/.local/bin
-
+set PATH $PATH ~/.local/bin
